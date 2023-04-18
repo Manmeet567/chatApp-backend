82a2b18f82784813e59915734bab27b693cd2caf
Manmeet567 <manmeet567singh@gmail.com>
2023-04-18 13:52:17 +0530

    created controller file for useProfile routes

---
+++ controllers/userProfileController.js
@@ -0,0 +1,20 @@
+const User = require('../models/userModel')
+
+// update status
+const updateStatus = async (req, res) => {
+    const user_id = req.user._id
+    const {status} = req.body;
+    try{
+        const updatedUserStatus = await User.findByIdAndUpdate(user_id, {status}, {new:true});
+
+        res.status(200).json({updatedUserStatus})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error:"Internal Server Error"})
+    }
+}
+
+
+module.exports = {
+    updateStatus
+}
--- routes/userProfileRoutes.js
+++ routes/userProfileRoutes.js
@@ -1,23 +1,12 @@
 const express = require('express')
-const User = require('../models/userModel')
 const requireAuth = require('../middleware/requireAuth')
+const {updateStatus} = require('../controllers/userProfileController')
 
 const router = express.Router()
 
 router.use(requireAuth)
 
-router.patch('/updateStatus',async (req,res) => {
-    const user_id = req.user._id
-    const {status} = req.body;
-    try{
-        const updatedUserStatus = await User.findByIdAndUpdate(user_id, {status}, {new:true});
-
-        res.status(200).json({updatedUserStatus})
-    }catch(error){
-        console.log(error)
-        res.status(500).json({error:"Internal Server Error"})
-    }
-})
+router.patch('/updateStatus', updateStatus)
 
 
 module.exports = router
