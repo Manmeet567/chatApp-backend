0170a1f6255223314cd20f9fa14af25a473255ea
Manmeet567 <manmeet567singh@gmail.com>
2023-04-19 12:47:48 +0530

    created more controller functions for pending and blocked

--- controllers/userProfileController.js
+++ controllers/userProfileController.js
@@ -18,19 +18,66 @@ const updateStatus = async (req, res) => {
 const getFriends = async (req,res) => {
     const {friends} = req.body;
 
-    try{
-        const users = await User.find({_id: {$in: friends}});
+    if(friends && friends.length === 0) {
+        res.status(200).json([]);
+    }
+    else{
+        try{
+            const users = await User.find({_id: {$in: friends}});
         
-        res.status(200).json(users)
-    }catch(error){
-        res.status(400).json({error:error})
+            res.status(200).json(users)
+        }
+        catch(error){
+            res.status(400).json({error:error})
+        }
     }
 
+}
+
+// get pending Friend List
+const getPendingUsers = async (req,res) => {
+    const {pending} = req.body;
+    if(pending && pending.length === 0) {
+        res.status(200).json([]);
+    }
+    else{
+        try{
+            const users = await User.find({_id: {$in: pending}});
+
+            res.status(200).json(users)
+        }
+        catch(error){
+            console.log(error)
+            res.status(400).json({error:error})
+        }
+    }
+}
+
+
+// get blocked users
+const getBlockedUsers = async (req,res) => {
+    const {blocked} = req.body;
+    if(blocked && blocked.length === 0){
+        res.status(200).json([]);
+    }
+    else{
+        try{
+            const users = await User.find({_id: {$in: blocked}});
+
+            res.status(200).json(users)
+        }
+        catch(error){
+            console.log(error)
+            res.status(400).json({error:error})
+        }
+    }
 }
 
 
 
 module.exports = {
     updateStatus,
-    getFriends
+    getFriends,
+    getPendingUsers,
+    getBlockedUsers
 }
--- routes/userProfileRoutes.js
+++ routes/userProfileRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const requireAuth = require('../middleware/requireAuth')
-const {updateStatus, getFriends} = require('../controllers/userProfileController')
+const {updateStatus, getFriends, getPendingUsers, getBlockedUsers} = require('../controllers/userProfileController')
 
 const router = express.Router()
 
 router.use(requireAuth)
 
 router.post('/friends', getFriends)
+
+router.post('/pending', getPendingUsers)
+
+router.post('/blocked', getBlockedUsers)
 
 router.patch('/updateStatus', updateStatus)
 
