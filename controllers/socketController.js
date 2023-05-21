5d6f05cb6287f96bbbf5b5a56a8d17f5e4aee26b
Manmeet567 <manmeet567singh@gmail.com>
2023-05-21 15:53:13 +0530

    created new file which will hold controller function of socket routes

---
+++ controllers/socketController.js
@@ -0,0 +1,28 @@
+const User = require('../models/userModel')
+
+const newConnection = async (req,res) => {
+    const socketId = req.body.socketId;
+    console.log(socketId)
+  try {
+    const userId = req.user._id;
+    
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: { socketId } },
+      { new: true }
+    );
+    
+    if (!updatedUser) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    
+    return res.status(200).json({ message: 'Socket ID stored successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+module.exports = {
+    newConnection
+}
