81ae9dc78c13426d51cb101ba85aa7595e738561
Manmeet567 <manmeet567singh@gmail.com>
2023-06-20 21:24:14 +0530

    i see a lot of changes, i actually let the chatgpt or bard to rewrite the whole server.js code again, so ,pretty much alot of things are same, but some major changes can be seen

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express')
-require('dotenv').config()
-const mongoose = require('mongoose')
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const cors = require('cors');
 const { initializeSocket } = require('./socket/socket');
 
-
 const userRoutes = require('./routes/users');
 const userProfileRoutes = require('./routes/userProfileRoutes');
-const serverRoutes = require('./routes/serverRoutes')
+const serverRoutes = require('./routes/serverRoutes');
 const socketRoutes = require('./routes/socketRoutes');
-
 
 const app = express();
 const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+  },
+});
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODBURI;
@@ -22,28 +25,25 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.use('/api/user',userRoutes)
-app.use('/api/server', serverRoutes)
-app.use('/api/userProfile', userProfileRoutes)
-app.use('/api/webSocket', socketRoutes)
-
+app.use('/api/user', userRoutes);
+app.use('/api/server', serverRoutes);
+app.use('/api/userProfile', userProfileRoutes);
+app.use('/api/webSocket', socketRoutes);
 
 // socket.io
-initializeSocket(server)
-
-server.listen(8900,() => {
-   console.log('Socket.io connected at 8900')
-})
+initializeSocket(server); // Pass the socket.io instance to the initializeSocket function
+// Set the 'io' object in the app
+app.set('io', io);
 
 // connecting to database
 mongoose.set('strictQuery', true);
-mongoose.connect(MONGODB_URI)
- .then(() => {
-    app.listen(PORT, () => {
-        console.log('Connected to mongodb and listening on PORT:'+PORT);
-    })
- })
- .catch((err) => {
-    console.log(err)
- })
-
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log('Connected to mongodb and listening on PORT:' + PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
